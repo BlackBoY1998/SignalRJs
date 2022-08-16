cce54d5ca3b792300d1e31b5bb22c622d50aad93
Akash Gawde <akash.gawde@nexsus.local>
2022-08-16 16:41:06 +0530

    first commit

---
+++ Controllers/RequestSenderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingalRJs.Controllers
+{
+    public class RequestSenderController : Controller
+    {
+        public HubConnection hubconnection = null;
+        public IHubProxy HubProxy = null;
+        string SignalRCon = ConfigurationManager.AppSettings["SignalRHub"];
+        // GET: Chat
+        public ActionResult GetRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetRequest(FormCollection fc)
+        {
+            return View();
+        }
+    }
+}
